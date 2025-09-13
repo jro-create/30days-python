5a2774e866b606c56969380146cbddcdfcbd16b8
Juan C. Rolon Torres <juancarlos@Juans-MacBook-Pro.local>
2025-09-13 17:44:38 -0400

    Day2: Variables and data types practice

---
+++ day2_variables.py
@@ -0,0 +1,33 @@
+# day2_variables.py
+ # Day 2: Variables & Data Types
+
+# Strings (text)
+name = "Juan"
+language = "Python"
+print("My name is", name)
+print("I'm learning", language)
+
+# Integers (whole numbers)
+age = 27
+year = 2025
+print("I am", age, "years old.")
+print("The year is", year)
+
+# Floats (decimal numbers)
+height = 180.34
+weight = 81.6
+
+print("Height:", height, "meters")
+print("Weight:", weight, "kg")
+
+# Booleans (True/False)
+is_learning = True
+has_pet = False
+print("Learning Python?", is_learning)
+print("Has a pet?", has_pet)
+
+# Type checking
+print("Type of name:", type(name))
+print("Type of age:", type(age))
+print("Type of height:", type(height))
+print("Type of is_learning:", type(is_learning))
