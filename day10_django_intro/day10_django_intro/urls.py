fa76bbb0d82a2208f41e0064a7255385725e7060
Juan <juancarlos@Juans-MacBook-Pro.local>
2025-10-11 00:01:43 -0400

    Day 10: Set up first Django project and app with Hello Django view

---
+++ day10_django_intro/day10_django_intro/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from hello import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home),
+]
+
+from django.contrib import admin
+from django.urls import path
+from hello import views
+
