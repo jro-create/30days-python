a92c953317b24a67d806159d5babf9fa8e398d0c
Juan <juancarlos@Juans-MacBook-Pro.local>
2025-10-13 19:09:32 -0400

    Day 10 Cleanup: Removed duplicate 'blog' app folder after renaming to 'day10_blog'.

--- mini_projects/day10_django_intro/d10_blog/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class D10BlogConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'd10_blog'
--- mini_projects/day10_django_intro/day10_django_intro/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from d10_blog import views as d10_blog_views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', d10_blog_views.post_list, name='post_list'),
-]
-
