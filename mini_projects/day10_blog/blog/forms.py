718740c66451069719efec2117f239dab039a56b
Juan <juancarlos@Juans-MacBook-Pro.local>
2025-11-14 20:28:25 -0400

    Fix forms.py: remove stray bracket; add CommentForm alongside PostForm

--- mini_projects/day10_blog/blog/forms.py
+++ mini_projects/day10_blog/blog/forms.py
@@ -4,7 +4,7 @@ from .models import Post, Comment
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ["title", "content"]
+        fields = ["title", "content", "cover"]
 
 class CommentForm(forms.ModelForm):
     class Meta:
